3d12aae88b1ec2444625651267064fc12df05812
Leandro Lima <89042570+liornes@users.noreply.github.com>
2025-08-04 13:52:49 -0300

    base api fastify

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { fastify } from 'fastify';
+
+const server = fastify({logger: true});
+
+const teams = [
+    {id: 1, name: "McLaren", base: "Woking, United Kingdom"},
+    {id: 2, name: "Mercedes", base: "Brackley, United Kingdom"},
+    {id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom"}
+];
+
+const drivers = [
+    {id: 1, name: "Max Verstappen", team: "Red Bull racing"},
+    {id: 1, name: "Lewis Hamilton", team: "Ferrari"},
+    {id: 1, name: "Lando Norris", team: "mcLaren"},
+];
+server.get("/teams", async(request, response) => {
+    response.type("application/json").code(200);
+
+    return {teams};
+});
+
+server.get("/drivers", async(request, response) => {
+    response.type("application/json").code(200);
+
+    return {drivers};
+});
+
+server.listen({port: 3333}, () => console.log("Server Init"));
